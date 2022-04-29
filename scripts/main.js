7bbb7dc78299c66b12ae1930a6e8d6a3c499c21d
Zadronix <75026373+Zadronix@users.noreply.github.com>
2022-04-29 15:57:14 +0300

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,27 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/MSSSM-Ignition.jpg') {
+      myImage.setAttribute ('src','images/Dreamy-Wind-2.jpg');
+    } else {
+      myImage.setAttribute ('src','images/MSSSM-Ignition.jpg');
+    }
+}
+var myHeading = document.querySelector('h3');
+
+ function setUserName() {
+   var myName = prompt('Please enter your name.');
+   localStorage.setItem('name', myName);
+   myHeading.textContent = 'Let`s breake this silence, ' + myName;
+  }
+  if(!localStorage.getItem('name')) {
+  setUserName();
+  } 
+  else {
+   var storedName = localStorage.getItem('name');
+   myHeading.textContent = 'Let`s breake this silence, ' + storedName;
+  }
+ myHeading.onclick = function() {
+  setUserName();
+}
